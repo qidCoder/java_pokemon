e315777fcb947ba6d8a1ce923fca7de2cd72181a
Shelley Ophir <qidcoder@gmail.com>
2020-12-04 16:26:22 -0500

    Completed Abstract Class

--- src/pokemon/AbstractPokemon.java
+++ src/pokemon/AbstractPokemon.java
@@ -3,5 +3,14 @@ package pokemon;
 //Create an abstract class that only implements createPokemon and pokemonInfo(Pokemon pokemon) from the PokemonInterface.
 
 public abstract class AbstractPokemon {
-
+	//these are from the interface
+	//This method creates and returns Pokemon.
+	public Pokemon createPokemon(String name, int health, String type) {
+		Pokemon newPokemon = new Pokemon(name, health, type);
+		
+		return newPokemon;
+	}
+	//This method returns a String with the name, health, and type of the pokemon.
+	public abstract String pokemonInfo(Pokemon pokemon);
+	
 }
